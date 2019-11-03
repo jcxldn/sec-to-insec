e925869f1a1c5f7f910a813044c5bf22ab327e8a
James Cahill <jcahill773@gmail.com>
2019-11-03 11:08:34 +0000

    Initial version

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var proxy = require("http-proxy-middleware");
+const pj = require("./package.json");
+
+var app = express();
+
+// Fix missing slashes (e.g. "folder" >> "folder/")
+app.use(function(req, res, next) {
+  // if / not in path, and is not a file
+  if (
+    req.path.substr(-1, 1) != "/" &&
+    !req.path.includes(".") &&
+    req.path.length > 1
+  ) {
+    res.redirect(301, req.path + "/");
+  } else {
+    next();
+  }
+});
+
+app.use(
+  "/",
+  proxy({
+    router: function(req) {
+      return (
+        "https://" +
+        req.hostname
+          .toLowerCase()
+          .split(`.${process.env.hostname.toLowerCase()}`)[0]
+      );
+    },
+    target: "tld.invalid",
+    changeOrigin: true,
+    headers: { "X-User-Agent": pj.name + "/" + pj.version },
+    hostRewrite: true,
+    autoRewrite: true,
+    // Allow self-signed certs
+    secure: false,
+    followRedirects: true,
+    // Disable console logging
+    logLevel: "silent"
+  })
+);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log(`Using hostname prefix of: *.${process.env.hostname}`);
+console.log(`Listening on port ${port}`);
